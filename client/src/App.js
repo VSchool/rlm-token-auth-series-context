d94e0cc13a80ca1a3498370aab9b588dc80629c7
Maira <masoomro16@ymail.com>
2022-05-16 08:12:42 -0700

    updated all packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Navbar from './components/Navbar.js'
 import Auth from './components/Auth.js'
 import Profile from './components/Profile.js'
@@ -10,21 +10,21 @@ export default function App(){
   const { token, logout } = useContext(UserContext)
   return (
     <div className="app">
-      <Navbar logout={ logout }/>
-      <Switch>
+      <Navbar logout={logout}/>
+      <Routes>
         <Route 
-          exact path="/" 
-          render={()=> token ? <Redirect to="/profile"/> : <Auth />}
+          path="/" 
+          element={ token ? <Navigate to="/profile"/> : <Auth />}
         />
         <Route 
           path="/profile"
-          render={() => <Profile />}
+          element={<Profile />}
         />
         <Route 
           path="/public"
-          render={() => <Public />}
+          element={<Public />}
         />
-      </Switch>
+      </Routes>
     </div>
   )
 }
